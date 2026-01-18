f5c8f77a5dd426eb8ee044d911b895a8e61f00af
steve <soocoolkim@gmail.com>
2026-01-18 12:00:56 +0900

    PWA

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['vite.svg', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Justran PWA',
+        short_name: 'Justran',
+        description: 'Justran Progressive Web App',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   server: {
     host: true,
   },
